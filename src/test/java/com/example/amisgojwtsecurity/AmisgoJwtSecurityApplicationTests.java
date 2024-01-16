c8cd841f0ffa9b4c6e09a6790c202ed162e404c3
beckbolot <bekoxna@gmail.com>
2024-01-16 17:05:46 +0300

    Initial commit

---
+++ src/test/java/com/example/amisgojwtsecurity/AmisgoJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amisgojwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmisgoJwtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
